0bb4ceca0d16d9e88e41ab58c34df608ecd294b5
odogeorge98 <odogeorge98@gmail.com>
2024-07-31 10:41:46 +0100

    Updated website content

---
+++ Script/home.js
@@ -0,0 +1,28 @@
+
+document.getElementById("menuToggle").addEventListener("click", function () {
+  const menu = document.getElementById("overlayMenu");
+  const closeMenu = document.getElementById("closeMenu");
+  const menuToggle = document.getElementById("menuToggle");
+
+  if (menu.classList.contains("show")) {
+      menu.classList.remove("show");
+      menuToggle.classList.remove("open"); // Rotate back to menu icon
+      closeMenu.classList.remove("show"); // Hide close icon
+      menuToggle.classList.add("show"); // Show menu icon
+  } else {
+      menu.classList.add("show");
+      menuToggle.classList.add("open"); // Rotate to 'X'
+      menuToggle.classList.remove("show"); // Hide menu icon
+      closeMenu.classList.add("show"); // Show close icon
+  }
+});
+
+document.getElementById("closeMenu").addEventListener("click", function () {
+  const menu = document.getElementById("overlayMenu");
+  const menuToggle = document.getElementById("menuToggle");
+
+  menu.classList.remove("show");
+  menuToggle.classList.remove("open"); // Rotate back to menu icon
+  menuToggle.classList.add("show"); // Show menu icon
+  this.classList.remove("show"); // Hide close icon
+});
